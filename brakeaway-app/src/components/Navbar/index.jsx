74501276056481673903b477583567bc43407bc2
Ghassan <g4ss4n@gmail.com>
2019-11-04 15:37:46 +0200

    menu works

--- brakeaway-app/src/components/Navbar/index.jsx
+++ brakeaway-app/src/components/Navbar/index.jsx
@@ -3,8 +3,39 @@ import "./style.css";
 import logo1 from "../../assets/images/Logo1.png";
 import logo2 from "../../assets/images/Logo2.png";
 
-
 export default class Navbar extends React.Component {
+
+  state = {
+    open: false,
+  };
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+
+  container = React.createRef();
+state = {
+  open: false,
+};
+
+handleClickOutside = event => {
+  if (this.container.current && !this.container.current.contains(event.target)) {
+    this.setState({
+      open: false,
+    });
+  }
+};
+
+componentDidMount() {
+  document.addEventListener("mousedown", this.handleClickOutside);
+}
+componentWillUnmount() {
+document.removeEventListener("mousedown", this.handleClickOutside);
+}
+
   render() {
     return (
       <section className="main-nav">
@@ -19,6 +50,23 @@ export default class Navbar extends React.Component {
           <a href="#contact">Contact Us</a>
         </div>
         <div>
+          <div className="iconcontainer" ref={this.container}>
+            <button type="button" class="icon" onClick={this.handleButtonClick}>
+              ☰
+            </button>
+{this.state.open && (
+  <div class="dropdown">
+    <ul>
+      <li > <a href="#home">Home</a></li>
+      <li > <a href="#about">About Us</a></li>
+      <li > <a href="#pb">Potential Benifits</a></li>
+      <li > <a href="#partner">Partner With Us</a></li>
+      <li > <a href="#team">Team</a></li>
+      <li > <a href="#contact">Contact Us</a></li>
+    </ul>
+  </div>
+)}
+          </div>
         </div>
       </section>
     );
